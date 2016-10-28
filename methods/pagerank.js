1bbe1167ed7181e5fd383010943f0acb566b93e0
Thomas BARRAS <thomas@MacBook-Pro-de-Thomas.local>
2016-10-27 20:43:54 -0400

    helper mongoose code scope

--- methods/pagerank.js
+++ methods/pagerank.js
@@ -13,40 +13,35 @@ mongoose.connection.on('open', function (err) {
 });
 
 /* defining helper fonctions */
-function helper() {
-    return 2;
+function getDampingFactor() {
+    return this.DAMPING_FACTOR;
 }
 
 /*
 * pagerank algorithm with mapReduce
 */
 /*
-PSEUDOCODE
 map( key: [url, pagerank], value: outlink_list )
-    for each outlink in outlink_list
-        emit( key: outlink, value: pagerank/size(outlink_list) )
-
-    emit( key: url, value: outlink_list )
+for each outlink in outlink_list
+    emit( key: outlink, value: pagerank/size(outlink_list) )
+emit( key: url, value: outlink_list )
 
 reducer( key: url, value: list_pr_or_urls )
-    outlink_list = []
-    pagerank = 0
-
-    for each pr_or_urls in list_pr_or_urls
-        if is_list( pr_or_urls )
-            outlink_list = pr_or_urls
-        else
-            pagerank += pr_or_urls
-
-    pagerank = 1 - DAMPING_FACTOR + ( DAMPING_FACTOR * pagerank )
-
-    emit( key: [url, pagerank], value: outlink_list )
+outlink_list = []
+pagerank = 0
+for each pr_or_urls in list_pr_or_urls
+if is_list( pr_or_urls )
+    outlink_list = pr_or_urls
+else
+    pagerank += pr_or_urls
+pagerank = 1 - DAMPING_FACTOR + ( DAMPING_FACTOR * pagerank )
+emit( key: [url, pagerank], value: outlink_list )
 */
 var o = {};
 o.map = function () {
     for (var i = 0, len = this.outlinkList.length; i < len; i++) {
         elt = this.outlinkList[i];
-        emit(elt, elt.weight / len);
+        emit(elt.id, elt.weight / len);
     }
     emit(this.id, this.outlinkList);
 };
@@ -56,13 +51,13 @@ o.reduce = function (k, vals) {
     for (var i = 0, len = vals.length; i < len; i++) {
         if (vals[i] instanceof Array)
             outlinkList = vals[i];
-        else
+        else 
             pagerank += vals[i];
     }
-    pagerank = 1 - DAMPING_FACTOR * (1 + pagerank);
+    pagerank = 1 - getDampingFactor() + getDampingFactor() * pagerank;
     return { key: [k, pagerank], value: outlinkList };
 };
-o.scope = { helper: new mongoose.mongo.Code(helper.toString()) }
+o.scope = { getDampingFactor: new mongoose.mongo.Code(getDampingFactor.toString()) }
 o.finalize = function (key, value) {
     if (typeof (value) != "number")
         value = 1;
@@ -70,9 +65,20 @@ o.finalize = function (key, value) {
     return value;
 };
 o.out = { "inline": 1 };
+/*
+for (var i = 1; i < 20; i++) {
+    Vertex.mapReduce(o, function (err, results) {
+        if (err) throw err;
+        console.log("iITERATION I : " + i);
+        console.log(results)
+    });
+}
+*/
+
 Vertex.mapReduce(o, function (err, results) {
     if (err) throw err;
-    console.log(results)
+    console.log(results);
 });
+
 
 mongoose.connection.close();
